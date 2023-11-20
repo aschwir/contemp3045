9fdde4acff99e24e4f1e4323e8878e1595c08196
andrew schwirzinski <aschwirzinski@gmail.com>
2023-11-20 12:30:29 -0500

    added Student.cs

---
+++ Final_Project/Final_Project/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿namespace Final_Project.Models
+{
+    public class Students
+    {
+        public int id { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+         public DateTime birthdate {  get; set; }
+        public string college_program {  get; set; }
+        public string year_in_program { get; set; }
+
+
+
+    }
+}
