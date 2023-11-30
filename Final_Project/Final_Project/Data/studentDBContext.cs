53c7a2251a125e025c5b5376814244c3247990f6
Kozim Kudratovic <kozimkuchkarov77@gmail.com>
2023-11-30 16:00:24 -0500

    #adding student

--- Final_Project/Final_Project/Data/studentDBContext.cs
+++ Final_Project/Final_Project/Data/studentDBContext.cs
@@ -18,7 +18,8 @@ namespace Final_Project.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Student>().HasData(
-                new Student {id= 1, fName = "Andrew", lName = "Schwirzinski", birthdate = new DateOnly(1980,07,19), college_program = "Information Technology", year_in_program = "junior" }
+                new Student {id= 1, fName = "Andrew", lName = "Schwirzinski", birthdate = new DateOnly(1980,07,19), college_program = "Information Technology", year_in_program = "junior" },
+                new Student { id = 1, fName = "Kozimjon", lName = "Kuchkorov", birthdate = new DateOnly(2002, 02, 27), college_program = "Information Technology", year_in_program = "pre-junior" }
               );
         }
 
