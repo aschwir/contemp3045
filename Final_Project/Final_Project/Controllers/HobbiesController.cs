0edbd82b129f854c88f26da47468126ad150b1a6
Ji'Yahna <jiymeade@gmail.com>
2023-12-03 17:01:42 -0500

    #Anything having to do with Hobby

---
+++ Final_Project/Final_Project/Controllers/HobbiesController.cs
@@ -0,0 +1,27 @@
+﻿using Final_Project.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project.Controllers
+{
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class HobbiesController : ControllerBase
+    {
+        private readonly ILogger<HobbiesController> _logger;
+        private readonly IHobbyContextDAO _context;
+
+        public HobbiesController(ILogger<HobbiesController> logger, IHobbyContextDAO context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.GetHobbies());
+        }
+
+    }
+}
---
+++ Final_Project/Final_Project/Models/Hobby.cs
@@ -0,0 +1,10 @@
+﻿namespace Final_Project.Models
+{
+    public class Hobby
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
